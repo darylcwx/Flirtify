65a2b134cc7164827c25632b09ed75e2a1c78640
waynekkkk <99787176+waynekkkk@users.noreply.github.com>
2023-04-02 02:37:44 +0800

    fix: added various configs for microservices

--- microservices/match.py
+++ microservices/match.py
@@ -13,7 +13,7 @@ import requests
 import json
 from datetime import datetime
 
-app = Flask(__name__, template_folder='../templates')
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config['SECRET_KEY'] = 'flirtify_esd_micro'
 app.config['SESSION_COOKIE_DOMAIN'] = '127.0.0.1'
 app.config['SESSION_COOKIE_PATH'] = '/'
